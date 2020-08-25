f06b22b360a161c1ad8c160559495fb5a5c9a6f4
Robert Kalescky <rkalescky@smu.edu>
2020-08-25 09:12:22 -0500

    Singularity build update.

--- environment.lua
+++ environment.lua
@@ -1,10 +1,10 @@
 always_load("singularity/3.5.3")
 
-local sif_hash = '0c2aab9fd6547957f880eaee8ff1f56fcc315dd406fad1ae3a28fc944ab6cd1b'
+local sif_hash = '13a3198450e46697948c7f946376272a9c14395a0c9e56656ad822fcd4f75a23'
 local sif_file = '/hpc/applications/singularity_containers/text_mining_with_python_sha256.' .. sif_hash .. '.sif'
 
 function build_command(app, sif)
-  local cmd        = 'singularity exec -B /scratch -B ${RSTUDIO_TMPDIR:-$(pwd)}:/tmp ' .. sif .. ' ' .. app
+  local cmd        = 'singularity exec -B /scratch ' .. sif .. ' ' .. app
   local sh_ending  = ' "$@"'
   local csh_ending = ' $*'
   local sh_cmd     = cmd .. sh_ending
@@ -16,8 +16,4 @@ setenv("TMPDIR", "/dev/shm")
 
 build_command('python3', sif_file)
 build_command('jupyter', sif_file)
-build_command('R', sif_file)
-build_command('RScript', sif_file)
-build_command('rserver', sif_file)
-build_command('rsession', sif_file)
 
