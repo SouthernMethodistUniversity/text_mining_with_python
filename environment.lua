dee5587412457acb90f831396e85fd790c6b1dda
Robert Kalescky <rkalescky@smu.edu>
2020-09-01 09:49:57 -0500

    Singularity build with NLTK datasets.

--- environment.lua
+++ environment.lua
@@ -1,6 +1,6 @@
 always_load("singularity/3.5.3")
 
-local sif_hash = '0548f36f51270f0fcb3b036ad67a2a063a014511075e46d79925cc3c112ca388'
+local sif_hash = 'b82f91c004dff207284e487b3095fb0cb0125b4dcad4925e3a96174e2455e8af'
 local sif_file = '/hpc/applications/singularity_containers/text_mining_with_python_sha256.' .. sif_hash .. '.sif'
 
 function build_command(app, sif)
