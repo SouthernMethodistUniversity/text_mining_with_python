52891230e4cc0509c1af2820d2c3f8d819b6c370
Robert Kalescky <rkalescky@smu.edu>
2020-10-14 10:26:12 -0500

    Set memory upper limit.

--- environment.lua
+++ environment.lua
@@ -1,6 +1,6 @@
 always_load("singularity/3.5.3")
 
-local sif_hash = '40b88131afd6462720fd48b4475f62783a46f1a8c69dc8376eb2f593be5c00f4'
+local sif_hash = '8db0437ec0108b1b59be6416bcf632948c51169f091e48a4cf4cebc896bbb50a'
 local sif_file = '/hpc/applications/singularity_containers/text_mining_with_python_sha256.' .. sif_hash .. '.sif'
 
 function build_command(app, sif)
@@ -13,6 +13,8 @@ function build_command(app, sif)
 end
 
 setenv("TMPDIR", "/dev/shm")
+local memory = os.getenv("SLURM_MEM_PER_NODE") or capture("awk '/MemTotal/{print $2}' /proc/meminfo") / 1024^2
+setenv("SINGULARITYENV_MEM_LIMIT", memory * 1024^2)
 
 build_command('python3', sif_file)
 build_command('jupyter', sif_file)
