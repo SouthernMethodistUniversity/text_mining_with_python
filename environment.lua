3dfd873a3ace296079b950a611e668f40ecb6b05
Robert Kalescky <rkalescky@smu.edu>
2021-01-28 15:07:15 -0600

    Remove multiple SIF functionality.

--- environment.lua
+++ environment.lua
@@ -3,12 +3,10 @@ always_load("singularity/3.5.3")
 local sif_hash = '850582113e30ea2701d4c85b3bb441de861d8a4960cf9c11cdccc332eba63546'
 local sif_file = '/hpc/applications/singularity_containers/text_mining_with_python_sha256.' .. sif_hash .. '.sif'
 
-function build_command(app, sif)
-  local cmd        = 'singularity exec -B /scratch ' .. sif .. ' ' .. app
-  local sh_ending  = ' "$@"'
-  local csh_ending = ' $*'
-  local sh_cmd     = cmd .. sh_ending
-  local csh_cmd    = cmd .. csh_ending
+function build_command(app)
+  local cmd        = 'singularity exec --writable-tmpfs -B /scratch,/work ' .. sif_file .. ' ' .. app
+  local sh_cmd     = cmd .. ' "$@"'
+  local csh_cmd    = cmd .. ' $*'
   set_shell_function(app , sh_cmd, csh_cmd)
 end
 
@@ -16,6 +14,6 @@ setenv("TMPDIR", "/dev/shm")
 local memory = os.getenv("SLURM_MEM_PER_NODE") or capture("awk '/MemTotal/{print $2}' /proc/meminfo") / 1024^2
 setenv("SINGULARITYENV_MEM_LIMIT", memory * 1024^2)
 
-build_command('python3', sif_file)
-build_command('jupyter', sif_file)
+build_command('python3')
+build_command('jupyter')
 
